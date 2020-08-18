086f304c9959ba950c5b8eba058c6b938f32c533
William Hall <billhall168@gmail.com>
2020-08-18 16:40:20 -0700

    add register and login pages, as well as forms on each. Update routes to match

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     res.render("home");
-})
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register")
+});
+
+app.get("/tenant", function (req, res) {
+    res.render("tenant")
+});
 
 
 
