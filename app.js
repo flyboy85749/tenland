509bbbcf1669f8f860c5cf96f622ba6a87640526
William Hall <billhall168@gmail.com>
2020-08-18 15:51:02 -0700

    added ejs views and partials folders, home page code

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.send("The server is running, and we are connected. WOO-HOO!");
+    res.render("home");
 })
 
 
