005c7731013a6bb05390cafeb8fdc39b7bdfe38c
William Hall <billhall168@gmail.com>
2020-08-24 15:52:51 -0700

    contact and register forms posting to console

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.get("/contact", function (req, res) {
     res.render("contact");
 });
 
+app.post("/contact", function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const comments =  req.body.comments;
+    console.log(name);
+    console.log(email);
+    console.log(comments);
+    res.redirect("thanks");
+});
+
 app.get("/login", function (req, res) {
     res.render("login");
     // if tenant, route to tenant page
@@ -34,12 +44,23 @@ app.get("/register", function (req, res) {
     res.render("register")
 });
 
+app.post("/register", function (req, res) {
+const email = req.body.email;
+const password = req.body.password;
+    console.log(email);
+    console.log(password);
+});
+
 app.get("/tenant", function (req, res) {
     res.render("tenant")
 });
 
 app.get("/admin", function (req, res) {
     res.render("admin")
+});
+
+app.get("/thanks", function (req, res) {
+    res.render("thanks");
 });
 
 
