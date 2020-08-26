68532336ac49dbad3b42cc7d2042cadc9600c3cf
William Hall <billhall168@gmail.com>
2020-08-26 14:46:18 -0700

    add google signin button

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -26,7 +27,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 mongoose.set("useCreateIndex", true);
 
@@ -36,19 +38,31 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/tenland"
+        callbackURL: "http://localhost:3000/auth/google/tenland",
+        userProfileURL: "https://googleapis.com/oauth2/v3/userinfo"
     },
     function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
         User.findOrCreate({
             googleId: profile.id
         }, function (err, user) {
@@ -60,6 +74,20 @@ passport.use(new GoogleStrategy({
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate('google', {
+        scope: ["profile"]
+    }));
+
+app.get("/auth/google/tenland",
+    passport.authenticate('google', {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect("/admin");
+    });
 
 app.get("/about", function (req, res) {
     res.render("about");
@@ -101,7 +129,12 @@ app.get("/tenant", function (req, res) {
 });
 
 app.get("/admin", function (req, res) {
-    res.render("admin")
+    if (req.isAuthenticated()) {
+        res.render("admin");
+    } else {
+        res.render("/login")
+    }
+
 });
 
 app.get("/thanks", function (req, res) {
